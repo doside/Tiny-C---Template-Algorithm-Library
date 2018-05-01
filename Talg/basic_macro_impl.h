2ed6c14bf4012beb6807dbf98acf2b1e7445cb95
Dennis <HenryBlue2333@mail.com>
2018-05-01 17:20:26 +0800

    增加一些说明.

--- Talg/basic_macro_impl.h
+++ Talg/basic_macro_impl.h
@@ -1,7 +1,15 @@
-
+﻿
 #define forward_m(...) std::forward<decltype(__VA_ARGS__)>(__VA_ARGS__)
+
+
+/*!
+	\brief	免于写noexcept(noexcept(...))
+	\note	不直接定义为 noexcept(noexcept(...)){ return ...},是为了便于调试
+*/
 #define except_when_m(...) noexcept(noexcept(__VA_ARGS__))
+
 #define omit_t_m(...) typename __VA_ARGS__::type
+
 #define declare_iterator_tpedef_m(T)	\
 using value_type		= typename T::value_type;		\
 using pointer			= typename T::pointer;			\
