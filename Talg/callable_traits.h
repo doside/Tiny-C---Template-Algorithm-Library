9a9cbd2dce53a2a07e752dba8274fe8163900abd
Dennis <HenryBlue2333@mail.com>
2018-05-01 16:29:10 +0800

    为ShortFirstCall LongFirstCall增加了文档. 并且允许LongFirstCall以无参数调用函子.

--- Talg/callable_traits.h
+++ Talg/callable_traits.h
@@ -59,9 +59,26 @@ struct isNonOverloadFunctor {
 
 
 
-
+/*!
+	\brief	采用链式继承来从F(T0,T1,...Tn)构造出一排重载
+			F(T0) 
+			F(T0,T1) 
+			F(T0,T1,T2)
+			.....
+			F(T0,T1,...,Tn)
+			参数个数最多的重载在派生链的最末端,从而被最优先选择.
+	\tparam	Ts...所有可能的参数类型
+*/
 template<class...Ts>struct LongFirstCall;
 
+
+
+/*!
+	\brief	采用链式继承来从F(T0,T1,...Tn)构造出一排重载
+			F(T0) F(T0,T1) F(T0,T1,T2).....F(T0,T1,...,Tn)
+			当前偏特化构造重载候选F(T<0>...T<k>),并且让基类继续构造重载候选F(T<0>...T<k-1>)
+	\tparam	Ts...当前的重载的参数 
+*/
 template<class...Ts>
 struct LongFirstCall<Seq<Ts...>> :public LongFirstCall<Pop_back_s<Seq<Ts...>>> {
 	using LongFirstCall<Pop_back_s<Seq<Ts...>>>::pcall;
@@ -74,47 +91,58 @@ struct LongFirstCall<Seq<Ts...>> :public LongFirstCall<Pop_back_s<Seq<Ts...>>> {
 		->Seq< decltype(forward_m(f)(forward_m(args)...)), Ts...>;
 	//返回的类型为 Seq<返回值类型, 参数类型>
 	//若这调用不合法那么一定会Substitution Failure，然后把指针的类型自动转换为基类指针
-
-
-	//fix: 若调用不是Substitution Failure而是Ambiguous的话,是否可以继续转换成基类指针再判定？
 	//todo: fix: vs2015 update2之前是无法在decltype内部使用pack expansion 的
 };
+
+/*!
+	\brief	采用链式继承来从F(T0,T1,...Tn)构造出一排重载
+			F(T0) F(T0,T1) F(T0,T1,T2).....F(T0,T1,...,Tn)
+			当前偏特化构造最后一个重载候选:F(),从而终止递归.
+*/
 template<>
 struct LongFirstCall<Seq<>> {
 	using self = LongFirstCall<Seq<>>;
 	template<class F, class...Us>
-	auto pcall(self*, F&& f, Us&&...)
-		//->decltype(f());
-		->Seq<>;
+	auto pcall(self*, F&& f, Us&&...)->Seq<decltype(f())>;	
 };
 
-template<class T>
-struct LongFirstCall<Seq<T>> :LongFirstCall<Seq<>> {
-	using LongFirstCall<Seq<>>::pcall;
-	using self = LongFirstCall<Seq<T>>;
-
-	template<class F, class...Us>
-	auto pcall(self*, F&& f, T&&arg, Us&&...)
-		//->decltype(f(arg));
-		->Seq<decltype(forward_m(f)(forward_m(arg))), T>;
-};
 
 struct LongParser
 {
 	template<class F, class...Ts>
 	auto operator()(F&& f, Ts&&...args)
-		-> decltype(LongFirstCall<Seq<Ts...>>{}
-	.pcall(
-		(LongFirstCall<Seq<Ts...>>*)nullptr,
-		forward_m(f), forward_m(args)...
-	));
+	->decltype(
+			LongFirstCall<Seq<Ts...>>{}.pcall(
+				(LongFirstCall<Seq<Ts...>>*)nullptr,
+				forward_m(f), forward_m(args)...
+			)
+	  );
 
-	template<class F>
-	Seq<> operator()(F&& f...);
+	template<class F,class...Us,int = 0>
+	Seq<> operator()(F&& f,Us&&...);	//用...的话会使得某些编译器引发意外的警告(如GCC). 
 };
 
+
+/*!
+	\brief	采用链式继承来从F(T0,T1,...Tn)构造出一排重载
+			F(T0) 
+			F(T0,T1) 
+			F(T0,T1,T2)
+			.....
+			F(T0,T1,...,Tn)
+			参数个数最少的重载在派生链的最末端,从而被最优先选择.
+	\tparam	Ts...所有可能的参数类型
+*/
 template<class...Ts>struct ShortFirstCall;
 
+/*!
+	\brief	采用链式继承来从F(T0,T1,...Tn)构造出一排重载
+			F(T0) F(T0,T1) F(T0,T1,T2).....F(T0,T1,...,Tn)
+			当前偏特化构造重载候选F(Ts...),并且让基类继续构造重载候选F(Ts...,U)
+	\tparam	Ts...当前的(也是直接基类的)重载的参数	
+	\tparam	U基类的重载的最后一个参数
+	\tparam	Us...剩下的参数,需要往上传递给其他基类.
+*/
 template<class...Ts, class U, class...Us>
 struct ShortFirstCall<Seq<Ts...>, U, Us...>
 	:ShortFirstCall< Seq<Ts..., U>, Us...>
@@ -122,20 +150,60 @@ struct ShortFirstCall<Seq<Ts...>, U, Us...>
 	using ShortFirstCall< Seq<Ts..., U>, Us...>::pcall;
 	using self = ShortFirstCall<Seq<Ts...>, U, Us...>;
 
+	/*!
+		\brief	重载候选
+		\tparam	Ps...被忽略的其他参数类型
+		\tparam	Ts...被使用的参数类型
+		\tparam	F 函子类型
+		\param	self* 用于产生重载优先级,因为重载的选择优先使用派生类
+		\param	f 被调用的函
+		\param	args...参数
+		\return	用于SFINAE.如果函子f可以以参数args...调用的话,
+				就把参数类型Ts...包装成Seq<R,Ts...>并返回,
+				其中R是f的返回类型.
+		\note	这个函数是不加实现的,主要用于探测重载.
+	*/
 	template<class F, class...Ps>
 	auto pcall(self*, F&& f, Ts&&...args, Ps&&...)
 		->Seq<decltype(forward_m(f)(forward_m(args)...)), Ts...>;
 };
 
+
+/*!
+	\brief	采用链式继承来从F(T0,T1,...Tn)构造出一排重载
+			F(T0) F(T0,T1) F(T0,T1,T2).....F(T0,T1,...,Tn)
+			当前偏特化构造最后一个重载候选,F(T0,...,Tn),从而终止递归.
+	\tparam	Ts...当前的重载的参数	
+*/
 template<class...Ts>
 struct ShortFirstCall<Seq<Ts...>> {
 	using self = ShortFirstCall<Seq<Ts...>>;
 
+	/*!
+		\brief	重载候选
+		\tparam	Ps...被忽略的其他参数类型
+		\tparam	Ts...被使用的参数类型
+		\tparam	F 函子类型
+		\param	self* 用于产生重载优先级,因为重载的选择优先使用派生类
+		\param	f 被调用的函
+		\param	args...参数
+		\return	用于SFINAE.如果函子f可以以参数args...调用的话,
+				就把参数类型Ts...包装成Seq<R,Ts...>并返回,
+				其中R是f的返回类型.
+		\note	这个函数是不加实现的,主要用于探测重载.
+				这里仍然有Ps...是因为Ts...可以是Ps...的真子集.
+	*/
 	template<class F, class...Ps>
 	auto pcall(self*, F&& f, Ts&&...args, Ps&&...)
 		->Seq<decltype(forward_m(f)(forward_m(args)...)), Ts...>;
 };
 
+
+
+/*!
+	\brief	用于启动ShortFirstCall来进行重载选择,以参数最短优先.
+	\see	ShortFirstCall
+*/
 struct ShortParser
 {
 	template<class F, class First, class...Ts>
