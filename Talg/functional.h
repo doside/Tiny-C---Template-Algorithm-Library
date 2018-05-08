8b9f420667ded7b50ba64ff65e31ef2e68b75f62
Dennis <HenryBlue2333@mail.com>
2018-05-08 18:37:45 +0800

    增添模块说明.

--- Talg/functional.h
+++ Talg/functional.h
@@ -1,36 +1,61 @@
-#pragma once
+﻿#pragma once
 #include "core.h"
 #include <type_traits>
 #include "basic_macro_impl.h"
-
+#include <functional>
+/*!
+	\module	Constexpr::Predicator
+	与标准库的相关设施的不同点在于返回值的自动推导.
+*/
 namespace Talg {
 	struct logical_and {
 		template<class T,class U>
-		constexpr decltype(auto) operator()(T&& lhs, U&& rhs)const {
+		constexpr auto operator()(T&& lhs, U&& rhs)const 
+		except_when_m(forward_m(lhs) && forward_m(rhs))
+		->decltype(forward_m(lhs) && forward_m(rhs))
+		{
 			return forward_m(lhs) && forward_m(rhs);
 		}
 	};
 	struct logical_or {
 		template<class T,class U>
-		constexpr decltype(auto) operator()(T&& lhs, U&& rhs)const {
+		constexpr auto operator()(T&& lhs, U&& rhs)const 
+		except_when_m(forward_m(lhs) || forward_m(rhs))
+		->decltype(forward_m(lhs) || forward_m(rhs))
+		{
 			return forward_m(lhs) || forward_m(rhs);
 		}
 	};
 	struct plus {
 		template<class T,class U>
 		constexpr auto operator()(T&& lhs, U&& rhs)const 
-		->decltype(forward_m(lhs) + forward_m(rhs)){
+		except_when_m(forward_m(lhs) + forward_m(rhs))
+		->decltype(forward_m(lhs) + forward_m(rhs))
+		//use trailing return type to supress the MSVC warning C4552
+		{			
 			return forward_m(lhs) + forward_m(rhs);
 		}
 	};
 	struct minus {
 		template<class T,class U>
 		constexpr auto operator()(T&& lhs, U&& rhs)const
-		->decltype(forward_m(lhs) - forward_m(rhs)){
+		except_when_m(forward_m(lhs) - forward_m(rhs))
+		->decltype(forward_m(lhs) - forward_m(rhs))
+		//use trailing return type to supress the MSVC warning C4552
+		{
 			return forward_m(lhs) - forward_m(rhs);
 		}
 	};
-	using equal_to = std::equal_to<void>;
+	struct equal_to {
+		template<class T,class U>
+		constexpr auto operator()(T&& lhs, U&& rhs)const 
+		except_when_m(forward_m(lhs) == forward_m(rhs))
+		->decltype(forward_m(lhs) == forward_m(rhs))
+		//use trailing return type to supress the MSVC warning C4552
+		{
+			return forward_m(lhs) == forward_m(rhs);
+		}
+	};
 
 }
 
